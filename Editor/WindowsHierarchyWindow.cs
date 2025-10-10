7e9dcc6530dcb6616fba94d64fff02c60f64a2cc
Derek Sliman <lionext.studio@gmail.com>
2025-10-10 18:22:54 +0300

    Added window hierarchy display in editor

---
+++ Editor/WindowsHierarchyWindow.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace TinyServices.Windows.Editor {
+    internal sealed class WindowsHierarchyWindow : EditorWindow {
+        private VisualElement _group;
+        
+        [MenuItem("Window/Tiny MVC/Windows Hierarchy", priority = 0)]
+        private static void OpenWindow() => GetWindow<WindowsHierarchyWindow>("Windows");
+        
+        private void OnBecameVisible() {
+            WindowsService.onShow.AddListener(UpdateWindowsList);
+            WindowsService.onHide.AddListener(UpdateWindowsList);
+        }
+        
+        private void OnBecameInvisible() {
+            WindowsService.onShow.RemoveListener(UpdateWindowsList);
+            WindowsService.onHide.RemoveListener(UpdateWindowsList);
+        }
+        
+        private void CreateGUI() {
+            VisualElement root = rootVisualElement;
+            ScrollView scroll = new ScrollView(ScrollViewMode.Vertical);
+            
+            _group = new VisualElement();
+            
+            _group.style.marginTop = 4f;
+            _group.style.marginBottom = 4f;
+            _group.style.marginLeft = 4f;
+            _group.style.marginRight = 4f;
+            
+            scroll.Add(_group);
+            root.Add(scroll);
+            
+            UpdateWindowsList();
+        }
+        
+        private void UpdateWindowsList() {
+            _group.contentContainer.Clear();
+            
+            if (WindowsService.visibleCount > 0) {
+                Type objectType = typeof(WindowBehavior);
+                
+                foreach (WindowBehavior window in WindowsService.ForeachVisible()) {
+                    ObjectField objectField = new ObjectField(window.ToString());
+                    objectField.objectType = objectType;
+                    objectField.value = window;
+                    objectField.SetEnabled(false);
+                    
+                    _group.contentContainer.Add(objectField);
+                }
+            } else {
+                TextElement label = new TextElement();
+                label.text = "Not have opened windows!";
+                _group.contentContainer.Add(label);
+            }
+        }
+    }
+}
